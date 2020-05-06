e68fd38253bcafe5fb8db9d840c3f9269febdf20
greekZorba <smiling.jinhak@gmail.com>
2020-05-06 23:33:49 +0900

    초기화 메소드(init)

---
+++ src/section4/init2.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+// main 함수보다 먼저 실행됨
+func init() {
+	fmt.Println("Init1 method start!")
+}
+
+func init() {
+	fmt.Println("Init2 method start!")
+}
+
+func init() {
+	fmt.Println("Init3 method start!")
+}
+
+func init() {
+	fmt.Println("Init4 method start!")
+}
+func main() {
+	// init: 패키지 로드 시에 가장 먼저 호출
+	// 가장 먼저 초기화 되는 작업 작성 시 유용하다
+	fmt.Println("Main method start!")
+}
--- src/section4/lib/lib.go
+++ src/section4/lib/lib.go
@@ -1,5 +1,11 @@
 package lib
 
+import "fmt"
+
+func init() {
+	fmt.Println("lib package Init Start!")
+}
+
 func CheckNum(c int32) bool {
 	return c > 10
 }
