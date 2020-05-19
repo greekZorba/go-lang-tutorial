8d9d63224a3617615270a512a272f4deac1f1d8c
greekZorba <smiling.jinhak@gmail.com>
2020-05-19 22:31:13 +0900

    포인터 기초

---
+++ src/section6/pointer3.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func rptc(n *int) {
+	*n = 77
+}
+
+func vptc(n int) {
+	n = 77
+}
+
+func main() {
+	// 포인터 값 전달
+	// 함수, 메서드 호출 시에 매개변수 값을 복사 전달 -> 함수, 메서드 내에서는 원본값 변경 불가능
+	// 원본 값 변경 위해서 포인터로 전달
+	// 특히, 크기가 큰 배열인 경우 값 복사시에 시스템 부담 -> 포인터 전달로 해결(슬라이스, 맵 참조 전달)
+
+	var a = 10
+	var b = 10
+
+	rptc(&a)
+	vptc(b)
+	fmt.Println("a : ", a)
+	fmt.Println("b : ", b)
+}
