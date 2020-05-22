27ebc584da5166dbeee870da96f06e6b7d66cc86
greekZorba <smiling.jinhak@gmail.com>
2020-05-22 18:18:58 +0900

    cluse 학습

--- src/section7/closure2.go
+++ src/section7/closure2.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	cnt := increaseCnt()
+
+	fmt.Println("cnt : ", cnt())
+	fmt.Println("cnt : ", cnt())
+	fmt.Println("cnt : ", cnt())
+	fmt.Println("cnt : ", cnt())
+	fmt.Println("cnt : ", cnt())
+	fmt.Println("cnt : ", cnt())
+	fmt.Println("cnt : ", cnt())
+}
+
+func increaseCnt() func() int {
+	n := 0 // 지역변수(캡처됨)
+	return func() int {
+		n += 1
+		return n
+	}
 }
